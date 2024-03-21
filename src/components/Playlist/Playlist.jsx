ce290ade59e769464e419345ba2e5440de12ce31
Smith Ringtho <sringtho@gmail.com>
2024-03-21 09:22:59 +0300

    Refactor: Removed repititive code in playlist component

--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -1,30 +1,24 @@
 import React from 'react'
 import './Playlist.scss'
-import Track from '../Track/Track'
+import Tracklist from '../Tracklist/Tracklist'
 import PlaylistName from '../PlaylistName/PlaylistName'
 
 const Playlist = ({ 
   playlist, 
   alterFunction, 
-  text, playlistName, 
+  text, 
+  playlistName, 
   setPlaylistName 
 }) => {
-  const playlistArr = playlist.map((track) => {
-    return (
-      <li key={`Track ${track.id}`}>
-        <Track track={track} alterFunction={alterFunction} text={text} />
-      </li>
-    )
-  })
 
   return (
     <div>
       <h1>Playlist</h1>
-      <PlaylistName 
-        playlistName={playlistName} 
-        setPlaylistName={setPlaylistName} 
+      <PlaylistName
+        playlistName={playlistName}
+        setPlaylistName={setPlaylistName}
       />
-      <ul>{playlistArr}</ul>
+      <Tracklist results={playlist} alterFunction={alterFunction} text={text} />
       <button>Save To Spotify</button>
     </div>
   )
